d2fa106ad92526e44e3e7c6a6da07177c5020053
tenbite-daniel <tenbitedaniel60@gmail.com>
2026-06-16 01:08:20 +0300

    feat: add CategoryGrid and CategoryCard components with i18n blurbs

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,9 +1,11 @@
 import HeroSlider from '@/components/home/HeroSlider';
+import CategoryGrid from '@/components/home/CategoryGrid';
 
 export default function Home() {
   return (
     <div>
       <HeroSlider />
+      <CategoryGrid />
     </div>
   );
 }
